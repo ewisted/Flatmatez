82021e3e59fd348fb82f416df82880f79f80fb11
Eric Wisted <diddin1@hotmail.com>
2019-06-25 20:41:34 -0500

    Added google image resources for the signing button

---
+++ Flatmatez/Flatmatez/Views/GroupSetup/GroupSetupPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Flatmatez.Views.GroupSetup
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class GroupSetupPage : ContentPage
+	{
+		public GroupSetupPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
